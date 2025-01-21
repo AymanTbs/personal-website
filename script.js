ab54bea02223f3c239c8854a5d9c9a77ec0a56bd
AymanTbs <ayman.trabelsi111@gmail.com>
2025-01-21 10:03:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ document.getElementById("quiz-form").addEventListener("submit", function (e) {
     q1: "a", // jaweb es7i7 lel question 1
     q2: "b", // jaweb es7i7 lel question 2
     q3: "b", // jaweb es7i7 lel question 3
+    q4: "c", // jaweb es7i7 lel question 4
+    q5: "b", // jaweb es7i7 lel question 5
+    q6: "b", // jaweb es7i7 lel question 6
+    q7: "a", // jaweb es7i7 lel question 7
+    q8: "c", // jaweb es7i7 lel question 8
+    q9: "b", // jaweb es7i7 lel question 9
+    q10: "b", // jaweb es7i7 lel question 10
   };
 
   for (let question in answers) {
